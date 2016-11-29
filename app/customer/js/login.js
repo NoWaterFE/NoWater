940228ff5a0cc6a5c42055c023ecd150d345daa1
HaishuoGao <gaohs7815@icloud.com>
2016-11-29 16:56:59 +0800

    error repair

--- app/customer/js/login.js
+++ app/customer/js/login.js
@@ -1,67 +1,77 @@
 var host="http://123.206.100.98:16120";
-var emailReg = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/i;
 var loginForm = $("#loginForm");
 loginForm.on("submit", function (e) {
     var _this = $(this);
+    var tips = showLoading(_this);
     e = window.event || e;
     if (e && e.preventDefault) {
         e.preventDefault();
     } else {
         e.returnValue = false;
     }
-    var name = this.userName.value, 
-        password = this.password.value; //username & password
+    var name = this.name.value, 
+        password = this.password.value; //username & password;
     if(!name){
         _this.find(".login").text("user name can't be empty!");
         return;
     }
-    if(!emailReg.test(name)){
-        _this.find(".login").text("error email!");
-        return;
-    }
-    //var tips = showLoading(_this);
+    var data = "name=" + name + "&password=" + $.md5(password);
     $.ajax({
         type: "post",
-        url: host+"customer/login",
+        url: host+"/customer/login",
         dataType: "json",
-        data: _this.serialize()     //序列化
+        data: data     //序列化
     }).done(function(result){
         if(tips) tips.remove();
         if(result.status==300){
-            _this.find(".login").text("No user or wrong password.");
-        } else if(result.status==200){
-            _this.find(".login").text("Login successfully.");
-            self.location = "index.html";
-        }
-    }).fail(function(result) {
-        if(tips) tips.remove();
-        result = {
-            status: 200
-        };
-       if(result.status==300){
            _this.find(".login").text("No user or wrong password.");
        } else if(result.status==200){
-           _this.find("input").addClass("disabled").attr("disabled", true);
            _this.find(".login").text("Login successfully.");
-           self.location = "index.html";
+           var url = getUrlParam("redirectUrl");
+           if(url) {
+            location.href = url;
+           } else {
+            location.href = "index.html";
+           }
        }
+    }).fail(function(result) {
+        if(tips) tips.remove();
+       //  result = {
+       //      status: 200
+       //  };
+       // if(result.status==300){
+       //     _this.find(".login").text("No user or wrong password.");
+       // } else if(result.status==200){
+       //     _this.find(".login").text("Login successfully.");
+       //     var url = getUrlParam("redirectUrl");
+       //     if(url) {
+       //      location.href = url;
+       //     } else {
+       //      location.href = "index.html";
+       //     }
+       // }
     });
 
 });
 
-// function showLoading($relative) {
-//     var $tips=$relative.siblings(".loadingImg");
-//     if($tips.length>0) $tips.remove();
-//     $tips= $("<div class='loadingImg'></div>");
-//     $tips.appendTo($relative.parent())
-//         .ready(function () {
-//             $tips.css({
-//                 "top": $relative.offset().top-$(window).scrollTop()+$relative.outerHeight()/2,
-//                 "left": $relative.offset().left-$(window).scrollLeft()+$relative.outerWidth()/2,
-//                 "margin-left": -$tips.outerWidth()/2,
-//                 "margin-top": -$tips.outerHeight()/2,
-//                 "visibility": "visible"
-//             });
-//         });
-//     return $tips;
-// }
+function getUrlParam(name) {
+   var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)"); //构造一个含有目标参数的正则表达式对象
+   var r = window.location.search.substr(1).match(reg); //匹配目标参数
+   if (r != null) return unescape(r[2]); return null; //返回参数值
+  }
+function showLoading($relative) {
+    var $tips=$relative.siblings(".loadingImg");
+    if($tips.length>0) $tips.remove();
+    $tips= $("<div class='loadingImg'></div>");
+    $tips.appendTo($relative.parent())
+        .ready(function () {
+            $tips.css({
+                "top": $relative.offset().top-$(window).scrollTop()+$relative.outerHeight()/2,
+                "left": $relative.offset().left-$(window).scrollLeft()+$relative.outerWidth()/2,
+                "margin-left": -$tips.outerWidth()/2,
+                "margin-top": -$tips.outerHeight()/2,
+                "visibility": "visible"
+            });
+        });
+    return $tips;
+}
--- app/customer/js/register.js
+++ app/customer/js/register.js
@@ -12,21 +12,18 @@ Dsy.prototype.Exists = function(id){
 function change(v){
 	var str="0";
 	for(i=0;i<v;i++){
-		str+=("_"+(document.getElementById(s[i]).selectedIndex-1));
+		str+=("_"+(document.getElementById(s[i]).selectedIndex));
 	};
 	var ss=document.getElementById(s[v]);
 	with(ss){
 		length = 0;
-		options[0]=new Option(opt0[v],opt0[v]);
-		if(v && document.getElementById(s[v-1]).selectedIndex>0 || !v){
-			if(dsy.Exists(str)){
-				ar = dsy.Items[str];
-				for(i=0;i<ar.length;i++){
-					options[length]=new Option(ar[i],ar[i]);
-				}//end for
-				if(v){ options[0].selected = true; }
-			}
-		}//end if v
+		if(dsy.Exists(str)){
+			ar = dsy.Items[str];
+			for(i=0;i<ar.length;i++){
+				options[length]=new Option(ar[i],ar[i]);
+			}//end for
+			options[0].selected = true;
+		}
 		if(++v<s.length){change(v);}
 	}//End with
 }
@@ -37,10 +34,9 @@ dsy.add("0",["HongkongIsland(HK)","NT_Island(NT)","Kowloon(KLN)"]);
 dsy.add("0_0",["Aberdeen", "Admiralty", "Ap Lei Chau", "Big Wave Bay", "Causeway Bay", "Central", "Central Sheung Wan", "Central South", "Chai Wan", "Gloucester Road", "Happy Valley", "Harbour Road", "Jardine's Lookout", "Kennedy Town", "Lai Tak Tsuen", "Mid-Levels", "Mid-Levels West", "North Point", "Pok Fu Lam", "Quarry Bay", "Sai Wan", "Shau Kei Wan", "Shek O", "Sheung Wan West", "Siu Sai Wan", "So Kon Po", "Southern District", "Stanley", "Tai Hang Road", "The Peak", "Tim Mei Ave", "Tin Hau", "Wah Fu", "Wan Chai", "Wong Chuk Hang"]);
 dsy.add("0_1",["Chek Lap Kok", "Chinese University", "Clear Water Bay", "Discovery Bay", "Fairview Park", "Fanling", "Fo Tan", "HKUST", "Kwai Chung", "Long Ping", "Ma On Shan", "Ma Wan", "Pat Heung", "Sai Kung (North)", "Sai Kung (South)", "Science Park", "Sha Tin", "Sheung Shui", "Siu Lek Yuen", "Tai Po", "Tai Wai", "Tin Shui Wai", "Tseung Kwan O", "Tsing Yi", "Tsuen Wan", "Tuen Mun", "Tung Chung", "Wu Kai Sha", "Yuen Long"]);
 dsy.add("0_2",["Cheung Sha Wan", "Choi Wan", "Diamond Hill", "Ho Man Tin", "Hung Hom", "Jordan", "Jordan Road", "Kowloon Bay", "Kowloon City", "Kowloon Tong", "Kwun Tong", "La Salle Road", "Lai Chi Kok", "Lam Tin", "Lok Fu", "Ma Tau Wai", "Mei Foo", "Mong Kok", "Ngau Chi Wan", "Ngau Tau Kok", "Rainbow Village", "San Po Kong", "Sau Mau Ping", "Sham Shui Po", "Shek Kip Mei", "Tai Kok Tsui", "To Kwa Wan", "Tsim Sha Tsui", "Tsz Wan Shan", "Wong Tai Sin", "Yau Ma Tei", "Yau Tong"]);
-dsy.add("0",["HongkongIsland(HK)","NT_Island(NT)","Kowloon(KLN)"]);
 
 var s=["area","district"];
-var opt0 = ["---Please choose your area---","---Please choose your district---"];
+
 function _init_area(){  
 	for(i=0;i<s.length-1;i++){
 	  document.getElementById(s[i]).onchange=new Function("change("+(i+1)+")");
@@ -50,45 +46,37 @@ function _init_area(){
 
 _init_area();
 
-var Gid  = document.getElementById ;
-var showArea = function(){
-	Gid('show').innerHTML =  Gid('area').value +  	
-	Gid('district').value + "</h3>"
-}
-Gid('area').setAttribute('onchange','showArea()');
-
 var host="http://123.206.100.98:16120";
-//var emailReg = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/i;
 var registerForm = $("#registerForm");
 registerForm.on("submit", function (e) {
     var _this = $(this);
+    var tips = showLoading(_this);
     e = window.event || e;
     if (e && e.preventDefault) {
         e.preventDefault();
     } else {
         e.returnValue = false;
     }
-    var name = this.userName.value,	//userName
+    var name = this.name.value,	//userName
         password = this.password.value,
         confirm = this.confirm.value,
-        telephone = this.phone.value,
+        telephone = this.telephone.value,
         address1 = this.area.value,	//area
         address2 = this.district.value,	//district
         address3 = this.address.value;	//address from input
-    // if(!emailReg.test(name)){
-    //     _this.find(".register").text("error email!");
-    //     return;
-    // }
     if (confirm != password) {
     	_this.find(".register").text("Please input the same password!");
     	return;
     }
-    // var tips = showLoading(_this);
+
+    var data = "name=" + name + "&password=" + $.md5(password) +"&telephone=" + telephone 
+    + "&address1=" + address1 +"&address2=" + address2 +"&address3=" + address3;
+ 
     $.ajax({
         type: "post",
-        url: host+"customer/register",
+        url: host+"/customer/register",
         dataType: "json",
-        data: _this.serialize()
+        data: data
     }).done(function(result){
         if(tips) tips.remove();
         if(result.status==200){
@@ -102,36 +90,36 @@ registerForm.on("submit", function (e) {
         }
     }).fail(function(result) {
         if(tips) tips.remove();
-        result = {
-            status: 200
-        };
-       if(result.status==200){
-        	_this.find("input").addClass("disabled").attr("disabled", true);
-            _this.find(".register").text("register successful.");
-        } else if(result.status==300){
-            _this.find(".register").text("user name has been used!");
-        } else if(result.status==400) {
-        	_this.find(".register").text("illegal telephone number!");
-        } else if (result.status==500) {
-        	_this.find(".register").text("illegal address!");
-        }
+       //  result = {
+       //      status: 200
+       //  };
+       // if(result.status==200){
+       //  	_this[0].reset();
+       //      _this.find(".register").text("register successful.");
+       //  } else if(result.status==300){
+       //      _this.find(".register").text("user name has been used!");
+       //  } else if(result.status==400) {
+       //  	_this.find(".register").text("illegal telephone number!");
+       //  } else if (result.status==500) {
+       //  	_this.find(".register").text("illegal address!");
+       //  }
     });
 
 });
 
-// function showLoading($relative) {
-//     var $tips=$relative.siblings(".loadingImg");
-//     if($tips.length>0) $tips.remove();
-//     $tips= $("<div class='loadingImg'></div>");
-//     $tips.appendTo($relative.parent())
-//         .ready(function () {
-//             $tips.css({
-//                 "top": $relative.offset().top-$(window).scrollTop()+$relative.outerHeight()/2,
-//                 "left": $relative.offset().left-$(window).scrollLeft()+$relative.outerWidth()/2,
-//                 "margin-left": -$tips.outerWidth()/2,
-//                 "margin-top": -$tips.outerHeight()/2,
-//                 "visibility": "visible"
-//             });
-//         });
-//     return $tips;
-// }
+function showLoading($relative) {
+    var $tips=$relative.siblings(".loadingImg");
+    if($tips.length>0) $tips.remove();
+    $tips= $("<div class='loadingImg'></div>");
+    $tips.appendTo($relative.parent())
+        .ready(function () {
+            $tips.css({
+                "top": $relative.offset().top-$(window).scrollTop()+$relative.outerHeight()/2,
+                "left": $relative.offset().left-$(window).scrollLeft()+$relative.outerWidth()/2,
+                "margin-left": -$tips.outerWidth()/2,
+                "margin-top": -$tips.outerHeight()/2,
+                "visibility": "visible"
+            });
+        });
+    return $tips;
+}
