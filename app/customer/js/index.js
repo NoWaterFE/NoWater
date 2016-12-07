a0441412c78e9777270d21e1b4e28b26c93cc499
dhgan <1562510684@qq.com>
2016-12-07 22:04:21 +0800

    修复confirm高度问题

--- app/customer/js/index.js
+++ app/customer/js/index.js
@@ -363,19 +363,43 @@ $adGoods.on("click", ".goods-item .add-to-favorites", function (event) {
 
 
 function tipsAlert(msg, callback){
-    var $alert = $(".alert");
+    var $alert = $(".tipsAlert");
     if ($alert.length > 0) $alert.remove();
-    $alert = $("<div class='alert'></div>");
+    $alert = $("<div class='tipsAlert'></div>");
     var $shadow = $("<div class='shadow'></div>");
     var $content = $("<div class='content'></div>");
     var $msg = $("<div class='msg'>"+ msg +"</div>");
     var $btn = $("<div class='btn'>OK</div>");
     $btn.on("click", function () {
-       $(this).parents(".alert").remove();
-       if(callback) callback();
+        $(this).parents(".tipsAlert").remove();
+        if(callback) callback();
     });
     $content.append($msg).append($btn);
     $alert.append($shadow);
     $alert.append($content);
     $alert.appendTo($("body"));
+}
+
+function tipsConfirm(msg, callback){
+    var $confirm = $(".tipsConfirm");
+    if ($confirm.length > 0) $confirm.remove();
+    $confirm = $("<div class='tipsConfirm'></div>");
+    var $shadow = $("<div class='shadow'></div>");
+    var $content = $("<div class='content'></div>");
+    var $msg = $("<div class='msg'>"+ msg +"</div>");
+    var $btn = $('<div class="btn2"> ' +
+        '<div class="cancel">Cancel</div> ' +
+        '<div class="ok">Ok</div> </div>');
+
+    $btn.on("click", ".cancel", function () {
+        $(this).parents(".tipsConfirm").remove();
+    });
+    $btn.on("click", ".ok", function () {
+        $(this).parents(".tipsConfirm").remove();
+        if(callback) callback();
+    });
+    $content.append($msg).append($btn);
+    $confirm.append($shadow)
+        .append($content)
+        .appendTo($("body"));
 }
--- app/seller/js/apply.js
+++ app/seller/js/apply.js
@@ -138,19 +138,43 @@ function showLoading($relative) {
 }
 
 function tipsAlert(msg, callback){
-    var $alert = $(".alert");
+    var $alert = $(".tipsAlert");
     if ($alert.length > 0) $alert.remove();
-    $alert = $("<div class='alert'></div>");
+    $alert = $("<div class='tipsAlert'></div>");
     var $shadow = $("<div class='shadow'></div>");
     var $content = $("<div class='content'></div>");
     var $msg = $("<div class='msg'>"+ msg +"</div>");
     var $btn = $("<div class='btn'>OK</div>");
     $btn.on("click", function () {
-        $(this).parents(".alert").remove();
+        $(this).parents(".tipsAlert").remove();
         if(callback) callback();
     });
     $content.append($msg).append($btn);
     $alert.append($shadow);
     $alert.append($content);
     $alert.appendTo($("body"));
+}
+
+function tipsConfirm(msg, callback){
+    var $confirm = $(".tipsConfirm");
+    if ($confirm.length > 0) $confirm.remove();
+    $confirm = $("<div class='tipsConfirm'></div>");
+    var $shadow = $("<div class='shadow'></div>");
+    var $content = $("<div class='content'></div>");
+    var $msg = $("<div class='msg'>"+ msg +"</div>");
+    var $btn = $('<div class="btn2"> ' +
+        '<div class="cancel">Cancel</div> ' +
+        '<div class="ok">Ok</div> </div>');
+
+    $btn.on("click", ".cancel", function () {
+        $(this).parents(".tipsConfirm").remove();
+    });
+    $btn.on("click", ".ok", function () {
+        $(this).parents(".tipsConfirm").remove();
+        if(callback) callback();
+    });
+    $content.append($msg).append($btn);
+    $confirm.append($shadow)
+        .append($content)
+        .appendTo($("body"));
 }
