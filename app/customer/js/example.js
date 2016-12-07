8950208f001739fc6b9977e6b8307b366faa1ee6
dhgan <1562510684@qq.com>
2016-12-07 21:21:16 +0800

    添加comfirm函数数

--- app/customer/js/example.js
+++ app/customer/js/example.js
@@ -123,3 +123,27 @@ function tipsAlert(msg, callback){
     $alert.append($content);
     $alert.appendTo($("body"));
 }
+
+function tipsConfirm(msg, callback){
+    var $confirm = $(".confirm");
+    if ($confirm.length > 0) $confirm.remove();
+    $confirm = $("<div class='confirm'></div>");
+    var $shadow = $("<div class='shadow'></div>");
+    var $content = $("<div class='content'></div>");
+    var $msg = $("<div class='msg'>"+ msg +"</div>");
+    var $btn = $('<div class="btn2"> ' +
+        '<div class="cancel">Cancel</div> ' +
+        '<div class="ok">Ok</div> </div>');
+
+    $btn.on("click", ".cancel", function () {
+        $(this).parents(".confirm").remove();
+    });
+    $btn.on("click", ".ok", function () {
+        $(this).parents(".confirm").remove();
+        if(callback) callback();
+    });
+    $content.append($msg).append($btn);
+    $confirm.append($shadow)
+        .append($content)
+        .appendTo($("body"));
+}
